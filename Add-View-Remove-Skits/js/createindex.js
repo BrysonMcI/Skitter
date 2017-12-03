b2d4ebb6f4067ec8f6eed3e5ace7ec601d452b86
Ohan <ohan@mail.rit.edu>
2017-12-03 04:41:28 -0500

    create index with mapping

--- Add-View-Remove-Skits/js/createindex.js
+++ Add-View-Remove-Skits/js/createindex.js
@@ -1,7 +1,21 @@
 var client = require('./connection.js');
 
 client.indices.create({
-	index: 'skitter'
+	index: 'skitter',
+	body: {
+		mappings: {
+			skit: {
+				properties: {
+					join_type: {
+						type: "join",
+						relations: {
+							top_level: "reply"
+						}
+					}
+				}
+			}
+		}
+	}
 }, function(err, resp, status) {
 	if(err) {
 		console.log(err);	
